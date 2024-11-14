3655fb740a199a0319fe0a4a695e375e7df3a22b
Peter Jaglom <peter@peterjaglom.com>
2024-11-13 20:10:33 -0800

    undid fetchGit code ... didn't work

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -40,32 +40,24 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  let
-    dotfiles = builtins.fetchGit {
-      url = "https://github.com/pjaglom/dotfiles.git";
-      ref = "main";
-  };
-  in
-  {
-    home.file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
 
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
-      ".zshrc".source = ${dotfiles}/zshrc/.zshrc;
-      ".config/alacritty".source = ../alacritty;
-      ".config/nvim".source = ../nvim;
-      ".config/nix".source = ../nix;
-      ".config/nix-darwin".source = ../nix-darwin;
-      #".config/tmux".source = ~/.dotfiles/tmux;
-    };
-  }
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    ".zshrc".source = ../zshrc/.zshrc;
+    ".config/alacritty".source = ../alacritty;
+    ".config/nvim".source = ../nvim;
+    ".config/nix".source = ../nix;
+    ".config/nix-darwin".source = ../nix-darwin;
+    #".config/tmux".source = ~/.dotfiles/tmux;
+  };
 
   # You can also manage environment variables but you will have to manually
   # source
