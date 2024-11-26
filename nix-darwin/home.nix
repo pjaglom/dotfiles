1e4db9b16128181ccaea1267a4cb0d022b772d3f
Peter Jaglom <peter@peterjaglom.com>
2024-11-25 16:29:25 -0800

    another p10k attempt

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -91,6 +91,18 @@
     '';
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
+    plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = ../p10k;
+          file = "p10k.zsh";
+        }
+    ]
     oh-my-zsh = {
       enable = true;
       plugins = [
@@ -103,16 +115,6 @@
         "copyfile"
         "copybuffer"
         "dirhistory"
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-          name = "powerlevel10k-config";
-          src = ../p10k;
-          file = "p10k.zsh";
-        }
       ];
     };
     shellAliases = {
