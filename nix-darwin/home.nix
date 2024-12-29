2ee953bf8852fc147eba1b96009bcc65058b4f9d
Peter Jaglom <peter@peterjaglom.com>
2024-12-28 20:36:05 -0500

    p10k worked somehow! re-enabling config

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -99,18 +99,18 @@
     '';
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
-    #plugins = [
-    #    {
-    #      name = "powerlevel10k";
-    #      src = pkgs.zsh-powerlevel10k;
-    #      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    #    }
-    #    {
-    #      name = "powerlevel10k-config";
-    #      src = ../p10k;
-    #      file = "p10k.zsh";
-    #    }
-    #];
+    plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = ../p10k;
+          file = "p10k.zsh";
+        }
+    ];
     oh-my-zsh = {
       enable = true;
       plugins = [
